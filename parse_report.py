673e733fc9c5de907215f1bca8c15776a983dd98
Boxing <boxingp@gmail.com>
2023-07-26 14:44:08 +0800

    AM-19: unify band of same notification email and take max value that is non-nan.

--- parse_report.py
+++ parse_report.py
@@ -25,11 +25,19 @@ def export_dataframe_to_excel(writer, dataframe, sheet_name, columns_width):
             worksheet.write(0, col_idx, col_name, header_format)
 
 
-def group_data(dataframe, group_column, ignore_column, notification_column):
+def filter_band(dataframe, notification_column, band_column, band: int):
+    dataframe[band_column] = dataframe[band_column].apply(pd.to_numeric, errors='coerce')
+    dataframe[band_column] = dataframe.groupby(notification_column)[band_column].transform('max')
+    dataframe = dataframe[(dataframe[band_column] < band) | dataframe[band_column].isna()].reset_index(drop=True)
+    return dataframe
+
+
+def group_data(dataframe, group_column, ignore_column, notification_column, band_column):
     filtered_df = dataframe.loc[dataframe[f'{group_column}邮箱'].notna() & (dataframe[f'{group_column}邮箱'] != '')]
     grouped_df = filtered_df.groupby(f'{group_column}邮箱', as_index=False).apply(
         lambda group: group.dropna(subset=['SN号'])).reset_index(drop=True)
-    grouped_df.rename(columns={f'{group_column}邮箱': notification_column, f'{group_column} Band': 'Band'}, inplace=True)
+    grouped_df.rename(columns={f'{group_column}邮箱': notification_column, f'{group_column} Band': band_column},
+                      inplace=True)
     grouped_df['Got from'] = group_column
     grouped_df.drop(columns=[f'{ignore_column}邮箱', f'{ignore_column} Band'], inplace=True)
     return grouped_df
@@ -48,19 +56,18 @@ def parse_report():
     origin_df = pd.read_excel(excel_file, sheet_name=os.getenv('REPORT_DATA_SHEET'))
     key_column = os.getenv('REPORT_PRIMARY_KEY')
     notification_column = os.getenv('REPORT_SEND_NOTIFICATION_TO_COLUMN')
+    band_column = 'Band'
     statistical_column = os.getenv('REPORT_STATISTICAL_COLUMN')
 
     selected_columns = os.getenv('REPORT_COLUMN').split(',')
     df = origin_df.loc[:, selected_columns]
     df = df[df[key_column].notna() & (df[key_column] != '')].reset_index(drop=True)
-    owner_group = group_data(df, 'Owner', 'User', notification_column)
+    owner_group = group_data(df, 'Owner', 'User', notification_column, band_column)
     owner_group_sn = owner_group[key_column].reset_index(drop=True)
     new_df = df[~df[key_column].isin(owner_group_sn)].reset_index(drop=True)
-    user_group = group_data(new_df, 'User', 'Owner', notification_column)
+    user_group = group_data(new_df, 'User', 'Owner', notification_column, band_column)
     processed_df = pd.concat([owner_group, user_group], ignore_index=True)
-    processed_df = processed_df[
-        (processed_df['Band'] < int(os.getenv('REPORT_IGNORED_BAND'))) | processed_df['Band'].isna()].reset_index(
-        drop=True)
+    processed_df = filter_band(processed_df, notification_column, band_column, int(os.getenv('REPORT_IGNORED_BAND')))
 
     selected_columns = [key_column, notification_column]
     final_df = origin_df.merge(processed_df[selected_columns], on=key_column, how='left')
