81c59c52df7e5c8ebcf5847d8ec91eaf6c81eb7b
Boxing <boxingp@gmail.com>
2023-07-26 11:29:25 +0800

    AM-16: store model column as variable in .env file.

--- send_notification.py
+++ send_notification.py
@@ -19,13 +19,14 @@ def send_notification():
     excel_file = pd.ExcelFile(report_path[0])
     df = pd.read_excel(excel_file, sheet_name=os.getenv('REPORT_RESULT_SHEET'))
     key_column = os.getenv('REPORT_PRIMARY_KEY')
+    model_column = os.getenv('REPORT_MODEL_COLUMN')
     notification_column = os.getenv('REPORT_SEND_NOTIFICATION_TO_COLUMN')
 
     df.replace({None: '', pd.NA: '', float('nan'): ''}, inplace=True)
     df = df[df[notification_column].notna() & (df[notification_column] != '')].reset_index(drop=True)
 
-    grouped_df = df.groupby('Send Notification To').apply(lambda group: group.loc[:, [key_column, 'Model']])
-    grouped_df.rename(columns={key_column: 'IT设备编号', 'Model': '型号'}, inplace=True)
+    grouped_df = df.groupby('Send Notification To').apply(lambda group: group.loc[:, [key_column, model_column]])
+    grouped_df.rename(columns={key_column: 'IT设备编号', model_column: '型号'}, inplace=True)
     grouped_df['是否在用此设备（是/否）'] = ''
     for email, info in grouped_df.groupby(level=0):
         info.reset_index(drop=True, inplace=True)
