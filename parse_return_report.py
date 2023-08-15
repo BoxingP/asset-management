90c1ef377d83266a37c4bdb8e641466707302177
Boxing <boxingp@gmail.com>
2023-08-15 16:27:12 +0800

    AM-26: update how to parse return report.

--- parse_return_report.py
+++ parse_return_report.py
@@ -36,13 +36,19 @@ def export_dataframe_to_excel(writer, dataframe, sheet_name):
         worksheet.write(0, col_idx, col_name, header_format)
 
 
-def filter_resignation_date(dataframe, target_month, target_year=datetime.datetime.now().year):
-    dataframe['离职日期'] = pd.to_datetime(dataframe['离职日期'])
-    return dataframe[(dataframe['离职日期'].dt.year == target_year) & (dataframe['离职日期'].dt.month == target_month)]
+def filter_resignation_date(dataframe, date_column, target_month, target_year=datetime.datetime.now().year):
+    dataframe[date_column] = pd.to_datetime(dataframe[date_column])
+    return dataframe[
+        (dataframe[date_column].dt.year == target_year) & (dataframe[date_column].dt.month == target_month)]
+
+
+def get_return_date(dataframe):
+    dataframe['待归还日期'] = dataframe['交接日期'].fillna(dataframe['离职日期'])
+    return dataframe
 
 
 def filter_return_state(dataframe, target_state):
-    return dataframe[dataframe['是否归还或转移日期'].str.contains(target_state)]
+    return dataframe[dataframe['归还情况'].str.contains(target_state)]
 
 
 def get_file_path_by_filename(paths, target_filename):
@@ -57,18 +63,20 @@ def parse_report():
     load_dotenv()
     report_folder_path = Path('/', *os.getenv('REPORT_FOLDER').split(',')).resolve()
     excel_file = pd.ExcelFile(Path(report_folder_path, os.getenv('RETURN_REPORT_NAME')))
-    origin_df = pd.read_excel(excel_file, sheet_name=os.getenv('RETURN_REPORT_DATA_SHEET'))
+    origin_df = pd.read_excel(excel_file, sheet_name=os.getenv('RETURN_REPORT_DATA_SHEET'),
+                              dtype={'员工号': str, '资产号': str, '序列号': str})
 
     selected_columns = os.getenv('RETURN_REPORT_COLUMN').split(',')
     df = origin_df.loc[:, selected_columns]
     df = filter_return_state(df, os.getenv('RETURN_REPORT_STATE'))
-    df = filter_resignation_date(df, target_month=int(os.getenv('RETURN_REPORT_MONTH')))
+    df = get_return_date(df)
+    df = filter_resignation_date(df, '待归还日期', target_month=int(os.getenv('RETURN_REPORT_MONTH')))
 
     output_folder_path = Path('/', *os.getenv('OUTPUT_FOLDER').split(',')).resolve()
     if not os.path.exists(output_folder_path):
         os.makedirs(output_folder_path)
     with pd.ExcelWriter(Path(output_folder_path, os.getenv('RETURN_REPORT_OUTPUT')), engine='xlsxwriter') as writer:
-        export_dataframe_to_excel(writer, df, 'test')
+        export_dataframe_to_excel(writer, df, os.getenv('RETURN_REPORT_OUTPUT_SHEET'))
 
 
 if __name__ == '__main__':
